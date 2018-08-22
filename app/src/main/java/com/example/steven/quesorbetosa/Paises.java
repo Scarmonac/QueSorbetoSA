5e15d3933b7a3fbb285b6917c109722f6786fa95
Scarmonac <stevencc@outlook.com>
2018-08-21 18:01:39 -0600

    works

---
+++ app/src/main/java/com/example/steven/quesorbetosa/Paises.java
@@ -0,0 +1,22 @@
+package com.example.steven.quesorbetosa;
+
+public class Paises {
+
+    private  String ContryName=null;
+
+    public Paises(String contryName) {
+        this.ContryName = contryName;
+    }
+
+    public Paises() {
+
+    }
+
+    public String getContryName() {
+        return ContryName;
+    }
+
+    public void setContryName(String contryName) {
+        this.ContryName = contryName;
+    }
+}
---
+++ app/src/main/java/com/example/steven/quesorbetosa/entidades/Producto.java
@@ -0,0 +1,46 @@
+package com.example.steven.quesorbetosa.entidades;
+
+public class Producto {
+
+
+
+    private Integer idd;
+    private String nombre;
+    private Integer precio_venta;
+
+
+    public Producto(Integer id, String nombre, Integer precio_venta) {
+        this.idd = idd;
+        this.nombre = nombre;
+        this.precio_venta = precio_venta;
+    }
+
+
+    public Producto(){
+
+    }
+
+    public Integer getId() {
+        return idd;
+    }
+
+    public void setId(Integer id) {
+        this.idd = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setTelefono(Integer precio_venta) {
+        this.precio_venta = precio_venta;
+    }
+}
